fc26af0dd8db9f8a480943498457d83593a98b8c
REZ_Aakash <aakash.pandey@rezolut.in>
2024-09-21 03:13:03 +0530

    updated

--- app/model/user_session.py
+++ app/model/user_session.py
@@ -5,6 +5,7 @@ class UserSession(BaseModel):
     __tablename__= "user_session"
     status = db.Column(db.String(50), nullable=True)
     session_key = db.Column(db.String(50), nullable=True)
+    user_id = db.Column(db.Integer, nullable=True)
     is_logged_out = db.Column(db.Boolean, nullable=False, default = False)
     logout_time = db.Column(db.DateTime, nullable=True)
     login_time = db.Column(db.DateTime, nullable=True)
